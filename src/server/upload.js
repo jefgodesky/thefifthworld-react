3caf58e7fb05750ecc3728afae1cef1015011675
Jason Godesky <jefgodesky@gmail.com>
2019-02-28 18:56:25 -0500

    Tweak how uploads are named and stored in S3

--- src/server/upload.js
+++ src/server/upload.js
@@ -11,10 +11,17 @@ const bucket = new aws.S3({ params: { Bucket: config.aws.bucket } })
 
 const upload = file => {
   return new Promise((resolve, reject) => {
+    const parts = file.name.split('.')
+    const nameParts = parts.slice(0, parts.length - 1)
+    const ext = parts[parts.length - 1]
     const stamp = new Date()
-    const y = stamp.getFullYear()
-    const m = `${stamp.getMonth() + 1}`.padStart(2, '0')
-    const name = `uploads/${y}/${m}/${file.name}`
+    const yr = stamp.getFullYear()
+    const mo = `${stamp.getMonth() + 1}`.padStart(2, '0')
+    const da = `${stamp.getDay()}`.padStart(2, '0')
+    const hr = `${stamp.getHours()}`.padStart(2, '0')
+    const mn = `${stamp.getMinutes()}`.padStart(2, '0')
+    const sc = `${stamp.getSeconds()}`.padStart(2, '0')
+    const name = `uploads/${nameParts.join('.')}.${yr}${mo}${da}.${hr}${mn}${sc}.${ext}`
 
     bucket.upload({
       Key: name,

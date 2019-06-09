e692dc023073ce66c6d87dd4fe2f47635c2d1328
Jason Godesky <jefgodesky@gmail.com>
2019-06-09 18:24:06 -0400

    Parse out links when doing a plain parse

--- src/server/parse/plain.js
+++ src/server/parse/plain.js
@@ -1,6 +1,26 @@
 import parse from './index'
 
 /**
+ * Parses links, but rather than actually creating links, just replaces them
+ * with their plain text values.
+ * @param txt {string} - Text to parse.
+ * @returns {*} - Parsed plain text.
+ */
+
+const plainParseLinks = txt => {
+  const links = txt.match(/\[\[(.*?)\]\]/g)
+  if (links) {
+    links.forEach(link => {
+      const full = link.substr(2, link.length - 4).trim()
+      const parts = full.split('|')
+      const text = parts.length > 1 ? parts[1] : full
+      txt = txt.replace(link, text)
+    })
+  }
+  return txt
+}
+
+/**
  * Returns the plain text of the parsed Markdown text, with all Markdown annd
  * other tags removed.
  * @param markdown {string} - Markdown text to parse.
@@ -9,8 +29,11 @@ import parse from './index'
  */
 
 const plainParse = async (markdown) => {
-  const parsed = await parse(markdown)
-  return parsed.replace(/<[^>]*>?/gm, '')
+  let plain = await parse(markdown)
+  plain = plain.replace(/<[^>]*>?/gm, '')
+  plain = plainParseLinks(plain)
+  plain = plain.replace(/\s+/g, ' ')
+  return plain
 }
 
 export default plainParse
--- src/server/parse/plain.spec.js
+++ src/server/parse/plain.spec.js
@@ -3,10 +3,17 @@
 import plainParse from './plain'
 
 describe('plainParse', () => {
-  it('strips out Markdown tags', async () => {
+  it('strips out Markdown', async () => {
     expect.assertions(1)
     const actual = await plainParse('This has **bolded text**, *italicized text*, and text that is ***both*** **bolded** ***and*** *italicized* ***at the same time.***')
     const expected = 'This has bolded text, italicized text, and text that is both bolded and italicized at the same time.'
+    expect(actual).toEqual(expected)
+  })
+
+  it('strips out links', async () => {
+    expect.assertions(1)
+    const actual = await plainParse('This has a [[simple link]] and a [[/path/to/something | more complex one]].')
+    const expected = 'This has a simple link and a more complex one.'
     expect(actual).toEqual(expected)
   })
 })

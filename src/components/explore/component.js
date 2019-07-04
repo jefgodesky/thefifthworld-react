d47ad66c438eaec45e0dac7c2b4c0609a53a67ea
Jason Godesky <jefgodesky@gmail.com>
2019-07-04 19:25:08 -0400

    Improve base experience

--- src/components/explore/component.js
+++ src/components/explore/component.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Header from '../header/component'
 import Footer from '../footer/component'
-import Map from '../map/component'
+// import Map from '../map/component'
 import autoBind from 'react-autobind'
 import { connect } from 'react-redux'
 
@@ -27,7 +27,18 @@ export class Explore extends React.Component {
     return (
       <React.Fragment>
         <Header name={loggedInMember && loggedInMember.name ? loggedInMember.name : null} />
-        <Map />
+        <main>
+          <h1>Explore</h1>
+          <p>Choose a continent:</p>
+          <ul>
+            <li><a href='/africa'>Africa</a></li>
+            <li><a href='/north-america'>North America</a></li>
+            <li><a href='/south-america'>South America</a></li>
+            <li><a href='/antarctica'>Antarctica</a></li>
+            <li><a href='/eurasia'>Eurasia</a></li>
+            <li><a href='/oceania'>Oceania</a></li>
+          </ul>
+        </main>
         <Footer />
       </React.Fragment>
     )

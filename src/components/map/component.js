3a00f3c652899463e95ef5e826c9733444d84fc9
Jason Godesky <jefgodesky@gmail.com>
2019-07-28 18:02:41 -0400

    Basic map functionality

--- src/components/map/component.js
+++ src/components/map/component.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios'
+import config from '../../../config'
 
 /**
  * This component handles interactive maps.
@@ -9,7 +11,9 @@ export default class Map extends React.Component {
     super(props)
 
     this.state = {
+      base: `https://s3.${config.aws.region}.amazonaws.com/${config.aws.bucket}/website/maps`,
       isClient: false,
+      json: [],
       lat: 0,
       lon: 0,
       zoom: 3
@@ -24,6 +28,51 @@ export default class Map extends React.Component {
 
   componentDidMount () {
     this.setState({ isClient: true })
+    this.loadGeoJSON()
+  }
+
+  /**
+   * Loads GeoJSON data.
+   * @returns {Promise<void>} - A promise that resolves when the necessary
+   *   GeoJSON data has been loaded into the state.
+   */
+
+  async loadGeoJSON () {
+    const { base } = this.state
+    const json = []
+    for (let i = 1; i <= 54; i++) {
+      const res = await axios.get(`${base}/5_json/ShapesNew_${i}.geojson`)
+      json[i] = res.data
+    }
+    this.setState({ json })
+  }
+
+  /**
+   * Returns a JSX array of GeoJSON components loaded with the data necessary
+   * to raise the sea level by 216 feet (the amount expected if all the ice
+   * caps melt).
+   * @param GeoJSON {Component} - The GeoJSON component from the
+   *   `react-leaflet` library.
+   * @returns {null|[]} - Either null if the needed GeoJSON data has not been
+   *   loaded yet, or an array of JSX GeoJSON components ready for rendering.
+   */
+
+  raiseSeaLevel (GeoJSON) {
+    const { json } = this.state
+    if (json && Array.isArray(json) && json.length > 0) {
+      const jsx = []
+      json.forEach((data, index) => {
+        jsx.push(
+          <GeoJSON
+            data={data}
+            key={index}
+            style={{ color: '#96B1CA', weight: 0, fillOpacity: 1.0 }} />
+        )
+      })
+      return jsx
+    } else {
+      return null
+    }
   }
 
   /**
@@ -34,20 +83,23 @@ export default class Map extends React.Component {
   render () {
     if (this.state.isClient) {
       const { lat, lon, zoom } = this.state
-      const pos = [ lat, lon ]
-
       const leaflet = require('react-leaflet')
       const LeafletMap = leaflet.Map
       const TileLayer = leaflet.TileLayer
+      const GeoJSON = leaflet.GeoJSON
+      const sealevel = this.raiseSeaLevel(GeoJSON)
 
       return (
         <LeafletMap
-          center={pos}
+          center={[lat, lon]}
           zoom={zoom}
-          minZoom={2}
+          minZoom={3}
+          maxZoom={15}
           style={{ width: '100%', height: '90vh' }}>
           <TileLayer
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+            url='http://b.tile.stamen.com/terrain-background/{z}/{x}/{y}.png' />
+          {sealevel}
         </LeafletMap>
       )
     } else {

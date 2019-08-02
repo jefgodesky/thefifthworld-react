123b9ee95fe41adf208a42c4efbd2a446e79492b
Jason Godesky <jefgodesky@gmail.com>
2019-08-02 17:44:12 -0400

    Center text in homepage sections

--- src/components/home/component.js
+++ src/components/home/component.js
@@ -29,9 +29,14 @@ export class Home extends React.Component {
           name={loggedInMember ? loggedInMember.name : null}
           addClasses={[ 'homepage' ]}
           skipNav />
-        <main>
-          <p>This is the homepage.</p>
-          <hr />
+        <main className='homepage'>
+          <section>
+            <h1>Humanity thrives beyond civilization.</h1>
+            <p><strong>The world has changed.</strong> The ruins of ancient cities lie submerged beneath the swollen seas. Beaches of translucent plastic sand mark new boundaries between land and water. Jungles stretch from the equator to the poles. Herds of elephants trample savannas in Canada and Russia. Tigers prowl the broken, overgrown ruins of London and Beijing. Tribes of hunter-gatherers roam the wilds. Villages cultivate forest gardens. The season of civilization has passed. Humanity has come home to a more-than-human world, but the bonds of kinship that provide them with everything they want or need in this feral future require constant care and attention lest they fray and break &mdash; as they nearly did in the ancient past.</p>
+            <p className='actions'>
+              <a href='/explore' className='button'>Explore</a>
+            </p>
+          </section>
           <PatreonLink />
         </main>
         <Footer />

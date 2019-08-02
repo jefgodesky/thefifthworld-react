a673276ee0ff005d265bfe5aaf021d7e704447a4
Jason Godesky <jefgodesky@gmail.com>
2019-08-01 20:02:35 -0400

    Cookie notice on login page
    Since we only use cookies for sessions, we don't need a pop-up

--- src/components/member-login/component.js
+++ src/components/member-login/component.js
@@ -25,28 +25,34 @@ export class MemberLogin extends React.Component {
         <Header />
         <main className='login'>
           <h1>Login</h1>
-          <form action='/login' method='post'>
-            <label htmlFor='email'>Email</label>
-            <input type='text' name='email' id='email' placeholder='you@example.com' />
-            <label htmlFor='passphrase'>Passphrase</label>
-            <input type='text' name='passphrase' id='passphrase' placeholder='Whisper your secret passphrase' />
-            <p className='actions'>
-              <button>Log in</button>
-            </p>
-            <p className='actions'>
-              <a href='/forgot-passphrase'>Forgot your passphrase?</a>
-            </p>
-          </form>
-          <div className='oauth2-login'>
-            <p>Or login with&hellip;</p>
-            <ul>
-              <li><a href='/login/patreon' className='button patreon'>Patreon</a></li>
-              <li><a href='/login/discord' className='button discord'>Discord</a></li>
-              <li><a href='/login/google' className='button google'>Google</a></li>
-              <li><a href='/login/facebook' className='button facebook'>Facebook</a></li>
-              <li><a href='/login/twitter' className='button twitter'>Twitter</a></li>
-            </ul>
-          </div>
+          <section className='login'>
+            <form action='/login' method='post'>
+              <label htmlFor='email'>Email</label>
+              <input type='text' name='email' id='email' placeholder='you@example.com' />
+              <label htmlFor='passphrase'>Passphrase</label>
+              <input type='text' name='passphrase' id='passphrase' placeholder='Whisper your secret passphrase' />
+              <p className='actions'>
+                <button>Log in</button>
+              </p>
+              <p className='actions'>
+                <a href='/forgot-passphrase'>Forgot your passphrase?</a>
+              </p>
+            </form>
+            <div className='oauth2-login'>
+              <p>Or login with&hellip;</p>
+              <ul>
+                <li><a href='/login/patreon' className='button patreon'>Patreon</a></li>
+                <li><a href='/login/discord' className='button discord'>Discord</a></li>
+                <li><a href='/login/google' className='button google'>Google</a></li>
+                <li><a href='/login/facebook' className='button facebook'>Facebook</a></li>
+                <li><a href='/login/twitter' className='button twitter'>Twitter</a></li>
+              </ul>
+            </div>
+          </section>
+          <section>
+            <h2>We use cookies</h2>
+            <p>We use <strong>cookies</strong> to track your session when you log in. We don&rsquo;t use this to track you, and we certainly don&rsquo;t sell your data to anyone. HTTP doesn&rsquo;t really have a memory all on its own (&ldquo;stateless,&rdquo; as programmers put it), so we use cookies to remember you and provide you access to all the members-only features on the site. Getting into the weeds, when you click &ldquo;Log in,&rdquo; our server interprets that as &ldquo;please give me a cookie.&rdquo;</p>
+          </section>
         </main>
         <Footer />
       </React.Fragment>

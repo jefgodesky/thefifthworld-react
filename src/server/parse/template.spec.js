d8171edca9ca2ff51abf232bc7e4d8fb54bc4211
Jason Godesky <jefgodesky@gmail.com>
2019-08-08 17:56:44 -0400

    Add unit tests for templates
    How were these not here before?!?

---
+++ src/server/parse/template.spec.js
@@ -0,0 +1,75 @@
+/* global describe, it, expect, beforeEach, afterEach, afterAll */
+
+import Member from '../../shared/models/member'
+import Page from '../../shared/models/page'
+import templateParse from './template'
+import db from '../db'
+
+beforeEach(async () => {
+  const tables = [ 'members', 'pages', 'changes' ]
+  for (const table of tables) {
+    await db.run(`DELETE FROM ${table};`)
+    await db.run(`ALTER TABLE ${table} AUTO_INCREMENT=1;`)
+  }
+
+  await db.run('INSERT INTO members (name, email, admin) VALUES (\'Admin\', \'admin@thefifthworld.com\', 1);')
+  await db.run('INSERT INTO members (name, email) VALUES (\'Normal\', \'normal@thefifthworld.com\');')
+})
+
+describe('templateParse', () => {
+  it('adds a template', async () => {
+    expect.assertions(1)
+    const member = await Member.get(2, db)
+    await Page.create({
+      title: 'Template:Hello',
+      body: '{{Template}}Hello world!{{/Template}} [[Type:Template]]'
+    }, member, 'Initial text', db)
+    const actual = await templateParse('{{Template:Hello}}', db)
+    expect(actual).toEqual('Hello world!')
+  })
+
+  it('can take a parameter', async () => {
+    expect.assertions(1)
+    const member = await Member.get(2, db)
+    await Page.create({
+      title: 'Template:Hello',
+      body: '{{Template}}Hello, {{{Name}}}!{{/Template}} [[Type:Template]]'
+    }, member, 'Initial text', db)
+    const actual = await templateParse('{{Template:Hello Name="Bob"}}', db)
+    expect(actual).toEqual('Hello, Bob!')
+  })
+
+  it('works with curly quotes', async () => {
+    expect.assertions(1)
+    const member = await Member.get(2, db)
+    await Page.create({
+      title: 'Template:Hello',
+      body: '{{Template}}Hello, {{{Name}}}!{{/Template}} [[Type:Template]]'
+    }, member, 'Initial text', db)
+    const actual = await templateParse('{{Template:Hello Name=”Bob”}}', db)
+    expect(actual).toEqual('Hello, Bob!')
+  })
+
+  it('works with line breaks between parameters', async () => {
+    expect.assertions(1)
+    const member = await Member.get(2, db)
+    await Page.create({
+      title: 'Template:Hello',
+      body: '{{Template}}Hello, {{{Name}}}!{{/Template}} [[Type:Template]]'
+    }, member, 'Initial text', db)
+    const actual = await templateParse('{{Template:Hello\n  Name=”Bob”}}', db)
+    expect(actual).toEqual('Hello, Bob!')
+  })
+})
+
+afterEach(async () => {
+  const tables = [ 'members', 'pages', 'changes' ]
+  for (const table of tables) {
+    await db.run(`DELETE FROM ${table};`)
+    await db.run(`ALTER TABLE ${table} AUTO_INCREMENT=1;`)
+  }
+})
+
+afterAll(() => {
+  db.end()
+})

66bd857a2e7a892f317f53dd0177b063c826dfa1
Jason Godesky <jefgodesky@gmail.com>
2019-09-29 14:15:37 -0400

    Move intro into its own component
    Each step will be its own component

--- src/components/community-creation/component.js
+++ src/components/community-creation/component.js
@@ -4,6 +4,8 @@ import Header from '../header/component'
 import Footer from '../footer/component'
 import Messages from '../messages/component'
 
+import CommunityCreationIntro from './steps/intro'
+
 import autoBind from 'react-autobind'
 import { connect } from 'react-redux'
 
@@ -36,10 +38,15 @@ export class CommunityCreation extends React.Component {
    */
 
   render () {
-    const { loggedInMember } = this.props
-    const js = !this.state.js
-      ? (<p>We highly recommend having JavaScript enabled for this process. You can do it without JavaScript, but you&rsquo;ll have a much harder time. In particular, you&rsquo;ll have to enter some latitude and longitude coordinates throughout, rather than just clicking on a map. If you&rsquo;ve turned JavaScript off, you might want to turn it on for this. If you haven&rsquo;t disabled JavaSCript, something else might have gone wrong to prevent it from working correctly. Try reloading the page &mdash; sometimes that can fix it.</p>)
-      : null
+    const { loggedInMember, step } = this.props
+
+    let body = null
+
+    switch (step) {
+      default:
+        body = (<CommunityCreationIntro js={this.state.js} />)
+        break
+    }
 
     return (
       <React.Fragment>
@@ -48,12 +55,7 @@ export class CommunityCreation extends React.Component {
           title='Create a community' />
         <main className='community-creation'>
           <Messages />
-          <p>Stories in the Fifth World &mdash; whether <a href='/stories'>written by a single person</a> or <a href='/rpg'>played out with a group</a> &mdash; center on <a href='/community'>communities</a>. This community creation wizard helps you create a community, working out its history and traditions and the people who make it up.</p>
-          <p>To properly <a href='/rpg/principles/heed-the-spirit-of-the-place'>heed the spirit of the place</a>, you&rsquo;ll need to deal with <a href='/place'>places</a> that you know personally and can go spend some time in, so typically you should create a community living in the place where you live. You should not create communities living in places where you don&rsquo;t have much personal experience (unless they lie in areas not much inhabited today, like the shores of the <a href='/arctic-ocean'>Arctic Ocean</a> or <a href='/antarctica'>Antarctica</a>). Leave space open for people who live in those places to tell us what they look like in the Fifth World. This means that you should rarely use this tool.</p>
-          {js}
-          <p className='actions'>
-            <button>Begin</button>
-          </p>
+          {body}
         </main>
         <Footer />
       </React.Fragment>
@@ -68,13 +70,16 @@ export class CommunityCreation extends React.Component {
  */
 
 const mapStateToProps = state => {
+  const { CommunityCreation } = state
   return {
-    loggedInMember: state.MemberLogin
+    loggedInMember: state.MemberLogin,
+    step: CommunityCreation.step
   }
 }
 
 CommunityCreation.propTypes = {
-  loggedInMember: PropTypes.object
+  loggedInMember: PropTypes.object,
+  step: PropTypes.number
 }
 
 export default connect(mapStateToProps)(CommunityCreation)

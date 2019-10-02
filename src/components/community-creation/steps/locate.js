a2aaf51d91e95853bc184900b7ed91efbe04e959
Jason Godesky <jefgodesky@gmail.com>
2019-10-01 22:57:38 -0400

    Error handling on manual form

--- src/components/community-creation/steps/locate.js
+++ src/components/community-creation/steps/locate.js
@@ -13,15 +13,43 @@ export class CommunityCreationLocate extends React.Component {
    */
 
   renderManual () {
+    const { error, lat, lon } = this.props.params
+
+    let latVal = lat ? decodeURIComponent(lat) : ''
+    const latError = error && [ 'both', 'lat' ].indexOf(error) > -1
+    const latErrorClass = latError ? 'error' : null
+    const latErrorText = latError
+      ? (<p className='error'>Sorry, we couldn&rsquo;t parse that as a latitude. You can enter it in a decimal format (like <em>19.692293</em>), or using degrees, minutes, and seconds (like <em>19&deg;41'32.3"N</em>.</p>)
+      : null
+
+    let lonVal = lon ? decodeURIComponent(lon) : ''
+    const lonError = error && [ 'both', 'lon' ].indexOf(error) > -1
+    const lonErrorClass = lonError ? 'error' : null
+    const lonErrorText = lonError
+      ? (<p className='error'>Sorry, we couldn&rsquo;t parse that as a longitude. You can enter it in a decimal format (like <em>-98.843654</em>), or using degrees, minutes, and seconds (like <em>98&deg;50'37.2"W</em>.</p>)
+      : null
+
     return (
       <React.Fragment>
         <h2>Where does your community dwell?</h2>
         <p>Provide latitude and longitude for a place near the center of your community&rsquo;s territory. Normally, wherever you find yourself right now would make for an excellent place to start.</p>
         <form action='/create-community/1' method='POST'>
-          <label htmlFor='lat'>Latitude</label>
-          <input type='text' name='lat' id='lat' />
-          <label htmlFor='lon'>Longitude</label>
-          <input type='text' name='lon' id='lon' />
+          <label htmlFor='lat' className={latErrorClass}>Latitude</label>
+          <input
+            type='text'
+            name='lat'
+            id='lat'
+            defaultValue={latVal}
+            className={latErrorClass} />
+          {latErrorText}
+          <label htmlFor='lon' className={lonErrorClass}>Longitude</label>
+          <input
+            type='text'
+            name='lon'
+            id='lon'
+            defaultValue={lonVal}
+            className={lonErrorClass} />
+          {lonErrorText}
           <button>Next</button>
         </form>
       </React.Fragment>
@@ -37,7 +65,7 @@ export class CommunityCreationLocate extends React.Component {
     // Temporary BS because this is just a placeholder right now and we're not
     // yet using the props we'll need later.
     if (this.props.js) console.log('has js')
-    if (this.props.dispatch) console.log(this.props.dispatch)
+    if (this.props.dispatch) console.log('has dispatch')
 
     const manual = this.renderManual()
 
@@ -51,6 +79,7 @@ export class CommunityCreationLocate extends React.Component {
 
 CommunityCreationLocate.propTypes = {
   dispatch: PropTypes.func,
+  params: PropTypes.object,
   js: PropTypes.bool
 }
 

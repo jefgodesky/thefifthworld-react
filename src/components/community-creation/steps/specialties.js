af0a06a4b2274c7b55042c8b27ed991a503b9cae
Jason Godesky <jefgodesky@gmail.com>
2019-10-10 15:26:57 -0400

    Don't allow more than four favorites

--- src/components/community-creation/steps/specialties.js
+++ src/components/community-creation/steps/specialties.js
@@ -15,13 +15,19 @@ export class CommunityCreationSpecialties extends React.Component {
    */
 
   renderOptions () {
-    const { options } = this.props
+    const { options, params } = this.props
+    const checked = params && params.specialties ? params.specialties : []
     if (isPopulatedArray(options)) {
       return options.map(option => {
         const slug = slugify(option)
         return (
           <li key={slug}>
-            <input type='checkbox' id={`specialty-${slug}`} name='specialty' value={option} />
+            <input
+              type='checkbox'
+              id={`specialty-${slug}`}
+              name='specialty'
+              value={option}
+              defaultChecked={checked.indexOf(option) > -1} />
             <label htmlFor={`specialty-${slug}`}>
               {option}
             </label>
@@ -39,11 +45,16 @@ export class CommunityCreationSpecialties extends React.Component {
    */
 
   render () {
-    const { id } = this.props
+    const { id, params } = this.props
+    const error = params && params.error && params.error === 'toomany'
+      ? (<p className='error'>It doesn&rsquo;t really count as a &ldquo;favorite&rdquo; if it includes everything, does it? Pick just four.</p>)
+      : null
+
     return (
       <React.Fragment>
         <h2>What does your community eat?</h2>
         <p>Nearly every community in the Fifth World hunts, gathers, and cultivates to one degree or another an enormous variety of plants, animals, and fungi for food, but they still have their favorites. <a href='/honey'>Honey</a> tops the list for almost everyone, and nearly goes without saying. What else does your family specialize in? You have a reputation when it comes to these food sources as local experts in how to hunt, gather, cultivate, or prepare them. Your neighbors come to you, often offering to trade their specialties for yours. Pick four.</p>
+        {error}
         <form action='/create-community' method='POST'>
           <input type='hidden' name='community' value={id} />
           <ul className='options very-short'>
@@ -60,7 +71,8 @@ export class CommunityCreationSpecialties extends React.Component {
 
 CommunityCreationSpecialties.propTypes = {
   id: PropTypes.string,
-  options: PropTypes.array
+  options: PropTypes.array,
+  params: PropTypes.object
 }
 
 export default CommunityCreationSpecialties

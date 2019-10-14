ee5f553b11538cc1264b5365688f39683c2abc56
Jason Godesky <jefgodesky@gmail.com>
2019-10-14 10:43:34 -0400

    Export default on class
    The step components are all dumb, so we don't need to do the complex exporting that we use for smart components.

--- src/components/community-creation/steps/intro.spec.js
+++ src/components/community-creation/steps/intro.spec.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import { shallow, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { CommunityCreationIntro } from './intro'
+import CommunityCreationIntro from './intro'
 
 configure({ adapter: new Adapter() })
 
--- src/components/community-creation/steps/specialties.spec.js
+++ src/components/community-creation/steps/specialties.spec.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import { shallow, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { CommunityCreationSpecialties } from './specialties'
+import CommunityCreationSpecialties from './specialties'
 
 configure({ adapter: new Adapter() })
 

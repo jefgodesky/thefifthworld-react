8549f9b80a4ab6d135831034640f24c63d7ee3b0
Jason Godesky <jefgodesky@gmail.com>
2019-11-03 14:36:30 -0500

    Fix linting errors

--- src/server/generate-community/shuffle.js
+++ src/server/generate-community/shuffle.js
@@ -1,9 +1,9 @@
 const shuffle = arr => {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+    [arr[i], arr[j]] = [arr[j], arr[i]]
   }
-  return arr;
+  return arr
 }
 
 export default shuffle

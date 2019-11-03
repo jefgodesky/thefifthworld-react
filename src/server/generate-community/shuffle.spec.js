db81e7cdbc2245eaac0a7f0f8a987e779a723172
Jason Godesky <jefgodesky@gmail.com>
2019-11-03 14:29:09 -0500

    Shuffle an array

---
+++ src/server/generate-community/shuffle.spec.js
@@ -0,0 +1,20 @@
+/* global describe, it, expect */
+
+import shuffle from './shuffle'
+
+describe('Shuffle', () => {
+  it('can randomly reorder an array', async () => {
+    const actual = shuffle([ 1, 2, 3, 4, 5 ])
+    const claims = [
+      actual.length === 5,
+      actual.indexOf(1) > -1,
+      actual.indexOf(2) > -1,
+      actual.indexOf(3) > -1,
+      actual.indexOf(4) > -1,
+      actual.indexOf(5) > -1
+    ]
+    console.log(actual)
+    const expected = claims.reduce((acc, curr) => acc && curr)
+    expect(expected).toEqual(true)
+  })
+})

03e4fd8e63e3014b5131a540b9e470b35aa251c1
Jason Godesky <jefgodesky@gmail.com>
2020-02-13 18:45:27 -0500

    Community class

---
+++ src/server/generate-community/community.js
@@ -0,0 +1,11 @@
+import { clone } from '../../shared/utils'
+
+export default class Community {
+  constructor (data) {
+    if (data) {
+      Object.keys(data).forEach(key => {
+        this[key] = clone(data[key])
+      })
+    }
+  }
+}
---
+++ src/server/generate-community/community.spec.js
@@ -0,0 +1,13 @@
+/* global describe, it, expect */
+
+import Community from './community'
+
+describe('Community', () => {
+  describe('constructor', () => {
+    it('copies data provided', () => {
+      const data = {test: {val: 57}}
+      const c = new Community(data)
+      expect(c.test.val).toEqual(57)
+    })
+  })
+})

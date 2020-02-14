184499a21d4ed62ffeb8601d30f323cef5ccfde9
Jason Godesky <jefgodesky@gmail.com>
2020-02-13 20:07:49 -0500

    History.combine

--- src/server/generate-community/history.js
+++ src/server/generate-community/history.js
@@ -1,4 +1,4 @@
-import { isPopulatedArray } from '../../shared/utils'
+import { dedupe, isPopulatedArray } from '../../shared/utils'
 
 export default class History {
   constructor () {
@@ -14,5 +14,33 @@ export default class History {
   add (year, event) {
     const existing = !isNaN(year) && isPopulatedArray(this.record[year]) ? this.record[year] : []
     this.record[year] = [ ...existing, event ]
+  }
+
+  /**
+   * Return a combined history.
+   * @param histories {[History]} - An array of History objects.
+   * @returns {History} - A single history that combines the events from the
+   *   histories given.
+   */
+
+  static combine (...histories) {
+    const arr = histories.filter(h => h.constructor && h.constructor.name === 'History')
+    const combined = new History()
+    arr.forEach(history => {
+      Object.keys(history.record).forEach(year => {
+        history.record[year].forEach(event => {
+          combined.add(year, event)
+        })
+      })
+    })
+
+    // There may be instances where the same event appears in more than one of
+    // the histories that we're combining. When that happens, we only want one
+    // instance of the event in our combined history.
+    Object.keys(combined.record).forEach(year => {
+      combined.record[year] = dedupe(combined.record[year])
+    })
+
+    return combined
   }
 }
--- src/server/generate-community/history.spec.js
+++ src/server/generate-community/history.spec.js
@@ -31,4 +31,25 @@ describe('History', () => {
       expect(h.record[2020][1].test).toEqual(2)
     })
   })
+
+  describe('combine', () => {
+    it('combines multiple histories', () => {
+      const h1 = new History()
+      const h2 = new History()
+      h1.add(2020, { test: 1 })
+      h2.add(2020, { test: 2 })
+      const h = History.combine(h1, h2)
+      expect(h.record[2020].length).toEqual(2)
+    })
+
+    it('eliminates duplicate events', () => {
+      const h1 = new History()
+      const h2 = new History()
+      h1.add(2020, { test: true })
+      h2.add(2020, { test: true })
+      const h = History.combine(h1, h2)
+      console.log(h.record)
+      expect(h.record[2020].length).toEqual(1)
+    })
+  })
 })

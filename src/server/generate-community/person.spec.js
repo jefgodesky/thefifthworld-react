95459c9b1f576ade333167fd89ad548a358979bb
Jason Godesky <jefgodesky@gmail.com>
2020-02-16 20:27:11 -0500

    Person.constructor: Add test for default date

--- src/server/generate-community/person.spec.js
+++ src/server/generate-community/person.spec.js
@@ -2,13 +2,19 @@
 
 import Person from './person'
 
-import { daysFromNow } from '../../shared/utils'
+import { daysFromNow, formatDate } from '../../shared/utils'
 
 describe('Person', () => {
   describe('constructor', () => {
     it('sets a birth date', () => {
       const p = new Person()
       expect(p.born instanceof Date).toEqual(true)
+    })
+
+    it('defaults birth date to 144,000 days from today', () => {
+      const p = new Person()
+      const expected = formatDate(daysFromNow(144000))
+      expect(formatDate(p.born)).toBe(expected)
     })
   })
 })

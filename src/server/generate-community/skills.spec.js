81e001ab7c480c440fe182549329d49b72689871
Jason Godesky <jefgodesky@gmail.com>
2020-02-20 20:45:05 -0500

    Skills class

---
+++ src/server/generate-community/skills.spec.js
@@ -0,0 +1,12 @@
+/* global describe, it, expect */
+
+import Skills from './skills'
+
+describe('Skills', () => {
+  describe('constructor', () => {
+    it('adds an array of skills that you\'ve mastered', () => {
+      const s = new Skills()
+      expect(s.mastered).toEqual([])
+    })
+  })
+})

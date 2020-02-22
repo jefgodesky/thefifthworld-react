53810617fe10d80c457d1ed5475a08baf78453c7
Jason Godesky <jefgodesky@gmail.com>
2020-02-21 21:00:52 -0500

    Remove a bunch of those Community.consider* methods I just did...
    We shouldn't be giving them their own methods. Magic is special. The rest of these should be weighted in the list of possibilities by adding them extra times, so that they're all weighted proportionately alongside other factors.

--- src/server/generate-community/skills.spec.js
+++ src/server/generate-community/skills.spec.js
@@ -142,119 +142,18 @@ describe('Skills', () => {
       expect(typeof Skills.considerMagic(p, c)).toEqual('boolean')
     })
 
+    it('returns false if the person already knows magic', () => {
+      const c = new Community()
+      const p = new Person(c)
+      p.skills.mastered.push('Magic')
+      expect(Skills.considerMagic(p, c)).toEqual(false)
+    })
+
     it('starts the person learning magic if it returns true', () => {
       const c = new Community()
       const p = new Person(c)
       const res = Skills.considerMagic(p, c)
       expect(!res || (res && p.skills.learning.skill === 'Magic')).toEqual(true)
-    })
-  })
-
-  describe('considerFavoredSkill', () => {
-    it('returns a boolean', () => {
-      const c = new Community()
-      const p = new Person(c)
-      expect(typeof Skills.considerFavoredSkill(p, c)).toEqual('boolean')
-    })
-
-    it('returns false if the community doesn\'t have a favored skill', () => {
-      const c = new Community()
-      const p = new Person(c)
-      expect(Skills.considerFavoredSkill(p, c)).toEqual(false)
-    })
-
-    it('starts the person learning the favored skill if it returns true', () => {
-      const c = new Community({ traditions: { skill: 'Unit testing' } })
-      const p = new Person(c)
-      const res = Skills.considerFavoredSkill(p, c)
-      expect(!res || (res && p.skills.learning.skill === 'Unit testing')).toEqual(true)
-    })
-
-    it('returns true more often if someone is agreeable', () => {
-      let count = 0
-      for (let i = 0; i < 100; i++) {
-        const c = new Community({ traditions: { skill: 'Unit testing' } })
-        const p = new Person(c)
-        p.personality.agreeableness = 1
-        if (Skills.considerFavoredSkill(p, c)) count++
-      }
-      expect(count).toBeGreaterThan(50)
-    })
-  })
-
-  describe('considerHealing', () => {
-    it('returns a boolean', () => {
-      const p = new Person()
-      expect(typeof Skills.considerHealing(p)).toEqual('boolean')
-    })
-
-    it('returns false if the person already knows medicine', () => {
-      const p = new Person()
-      p.skills.mastered.push('Medicine')
-      expect(Skills.considerHealing(p)).toEqual(false)
-    })
-
-    it('starts the person learning medicine if it returns true', () => {
-      const p = new Person()
-      const res = Skills.considerHealing(p)
-      expect(!res || (res && p.skills.learning.skill === 'Medicine')).toEqual(true)
-    })
-
-    it('returns true more often if someone is agreeable', () => {
-      let count = 0
-      for (let i = 0; i < 100; i++) {
-        const p = new Person()
-        p.personality.agreeableness = 1
-        if (Skills.considerHealing(p)) count++
-      }
-      expect(count).toBeGreaterThan(0)
-    })
-
-    it('returns true more often if the community has faced a lot of sickness recently', () => {
-      let count = 0
-      for (let i = 0; i < 100; i++) {
-        const p = new Person()
-        if (Skills.considerHealing(p, 10)) count++
-      }
-      expect(count).toBeGreaterThan(25)
-    })
-  })
-
-  describe('considerDeescalation', () => {
-    it('returns a boolean', () => {
-      const p = new Person()
-      expect(typeof Skills.considerDeescalation(p)).toEqual('boolean')
-    })
-
-    it('returns false if the person already knows deescalation', () => {
-      const p = new Person()
-      p.skills.mastered.push('Deescalation')
-      expect(Skills.considerDeescalation(p)).toEqual(false)
-    })
-
-    it('starts the person learning deescalation if it returns true', () => {
-      const p = new Person()
-      const res = Skills.considerDeescalation(p)
-      expect(!res || (res && p.skills.learning.skill === 'Deescalation')).toEqual(true)
-    })
-
-    it('returns true more often if someone is agreeable', () => {
-      let count = 0
-      for (let i = 0; i < 100; i++) {
-        const p = new Person()
-        p.personality.agreeableness = 1
-        if (Skills.considerDeescalation(p)) count++
-      }
-      expect(count).toBeGreaterThan(0)
-    })
-
-    it('returns true more often if the community has faced a lot of conflict recently', () => {
-      let count = 0
-      for (let i = 0; i < 100; i++) {
-        const p = new Person()
-        if (Skills.considerDeescalation(p, 10)) count++
-      }
-      expect(count).toBeGreaterThan(25)
     })
   })
 })

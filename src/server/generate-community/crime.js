4926d41301b1e143d55f9f1a0adc22a7936ee2c0
Jason Godesky <jefgodesky@gmail.com>
2020-02-22 11:13:34 -0500

    assault

--- src/server/generate-community/crime.js
+++ src/server/generate-community/crime.js
@@ -1,3 +1,7 @@
+import random from 'random'
+
+import { anyTrue, intersection, isPopulatedArray } from '../../shared/utils'
+
 /**
  * Consider assaulting or murdering someone.
  * @param attacker {Person} - The person who is considering an act of violence.
@@ -17,6 +21,33 @@ const considerViolence = (attacker) => {
   }
 }
 
+/**
+ * Figures out if an assault is successful or not.
+ * @param attacker {Person} - The person who is assaulting the defender.
+ * @param defender {Person} - The person being assaulted by the attacker.
+ * @returns {boolean} - `true` if the attacker succeeds in doing injury to the
+ *   defender, or `false` if the defender successfully fends her off.
+ */
+
+const assault = (attacker, defender) => {
+  const skillsForAttacker = [
+    'Deception', 'Sorcery', 'Running', 'Scouting', 'Archery', 'Throwing atlatl darts',
+    'Shooting a blowgun', 'Throwing a bola', 'Shooting a sling', 'Using a spear'
+  ]
+  const skillsForDefender = [ 'Running', 'Scouting' ]
+  const attackerSkills = intersection(attacker.skills.mastered, skillsForAttacker)
+  const defenderSkills = intersection(defender.skills.mastered, skillsForDefender)
+  let attackRolls = []
+  for (let a = 0; a < attackerSkills.length + 1; a++) {
+    const defenseRolls = []
+    for (let d = 0; d < defenderSkills.length + 1; d++) defenseRolls.push(random.boolean())
+    attackRolls.push(!anyTrue(defenseRolls))
+  }
+  attackRolls = attackRolls.sort(a => { return a ? -1 : 1 })
+  return isPopulatedArray(attackRolls) ? attackRolls[0] : false
+}
+
 export {
-  considerViolence
+  considerViolence,
+  assault
 }
--- src/server/generate-community/crime.spec.js
+++ src/server/generate-community/crime.spec.js
@@ -2,7 +2,10 @@
 
 import Person from './person'
 
-import { considerViolence } from './crime'
+import {
+  considerViolence,
+  assault
+} from './crime'
 
 describe('considerViolence', () => {
   it('returns a string', () => {
@@ -35,5 +38,55 @@ describe('considerViolence', () => {
       }
     }
     expect(stats.no).toBeLessThan(90)
+  })
+})
+
+describe('assault', () => {
+  it('returns a boolean', () => {
+    const attacker = new Person()
+    const defender = new Person()
+    expect(typeof assault(attacker, defender)).toEqual('boolean')
+  })
+
+  it('succeeds more than 25% of the time with average people', () => {
+    let count = 0
+    for (let i = 0; i < 100; i++) {
+      const attacker = new Person()
+      const defender = new Person()
+      if (assault(attacker, defender)) count++
+    }
+    expect(count).toBeGreaterThan(25)
+  })
+
+  it('succeeds less than 75% of the time with average people', () => {
+    let count = 0
+    for (let i = 0; i < 100; i++) {
+      const attacker = new Person()
+      const defender = new Person()
+      if (assault(attacker, defender)) count++
+    }
+    expect(count).toBeLessThan(75)
+  })
+
+  it('succeeds more often when the attacker has more deadly skills', () => {
+    let count = 0
+    for (let i = 0; i < 100; i++) {
+      const attacker = new Person()
+      const defender = new Person()
+      attacker.skills.mastered.push('Deception','Running', 'Scouting', 'Archery')
+      if (assault(attacker, defender)) count++
+    }
+    expect(count).toBeGreaterThan(75)
+  })
+
+  it('succeeds less often when the defender is properly skilled', () => {
+    let count = 0
+    for (let i = 0; i < 100; i++) {
+      const attacker = new Person()
+      const defender = new Person()
+      defender.skills.mastered.push('Running', 'Scouting')
+      if (assault(attacker, defender)) count++
+    }
+    expect(count).toBeLessThan(50)
   })
 })

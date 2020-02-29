716bcd9d90bbf32c8cce1c2edebbad6dfde371bd
Jason Godesky <jefgodesky@gmail.com>
2020-02-29 09:38:57 -0500

    commitAdultery

--- src/server/generate-community/love.spec.js
+++ src/server/generate-community/love.spec.js
@@ -1,8 +1,9 @@
 /* global describe, it, expect */
 
+import Community from './community'
 import Person from './person'
 
-import { encounter } from './love'
+import { encounter, commitAdultery } from './love'
 
 describe('encounter', () => {
   it('returns a boolean for mutual sexual attraction', () => {
@@ -89,5 +90,93 @@ describe('encounter', () => {
     const b = new Person()
     const report = encounter(a, b)
     expect(typeof report.b.other).toEqual('boolean')
+  })
+})
+
+describe('commitAdultery', () => {
+  it('adds an incident of adultery to the polycule history', () => {
+    const community = new Community()
+    const a = new Person(community)
+    const b = new Person()
+    const c = new Person()
+    const id = community.startPolycule(a, b)
+    commitAdultery(a, c, community)
+    expect(community.polycules[id].history.get({ tag: 'adultery' })).toHaveLength(1)
+  })
+
+  it('sometimes results in the polycule breaking up', () => {
+    let count = 0
+    for (let i = 0; i < 100; i++) {
+      const community = new Community()
+      const a = new Person(community)
+      const b = new Person()
+      const c = new Person()
+      const d = new Person()
+      const id = community.startPolycule(a, b, c)
+      commitAdultery(c, d, community)
+      if (community.polycules[id].history.get({ tag: 'breakup' }).length > 0) count++
+    }
+    expect(count).toBeGreaterThan(0)
+  })
+
+  it('results in the polycule breaking up less than 25% of the time', () => {
+    let count = 0
+    for (let i = 0; i < 100; i++) {
+      const community = new Community()
+      const a = new Person(community)
+      const b = new Person()
+      const c = new Person()
+      const d = new Person()
+      const id = community.startPolycule(a, b, c)
+      commitAdultery(c, d, community)
+      if (community.polycules[id].history.get({ tag: 'breakup' }).length > 0) count++
+    }
+    expect(count).toBeLessThanOrEqual(25)
+  })
+
+  it('sometimes results in the adulterers being removed', () => {
+    let count = 0
+    for (let i = 0; i < 100; i++) {
+      const community = new Community()
+      const a = new Person(community)
+      const b = new Person()
+      const c = new Person()
+      const d = new Person()
+      const id = community.startPolycule(a, b, c)
+      commitAdultery(c, d, community)
+      if (community.polycules[id].history.get({ tag: 'contracted' }).length > 0) count++
+    }
+    expect(count).toBeGreaterThan(0)
+  })
+
+  it('results in the adulterers being removed less than 35% of the time', () => {
+    let count = 0
+    for (let i = 0; i < 100; i++) {
+      const community = new Community()
+      const a = new Person(community)
+      const b = new Person()
+      const c = new Person()
+      const d = new Person()
+      const id = community.startPolycule(a, b, c)
+      commitAdultery(c, d, community)
+      if (community.polycules[id].history.get({ tag: 'contracted' }).length > 0) count++
+    }
+    expect(count).toBeLessThanOrEqual(35)
+  })
+
+  it('usually results in the polycule recovering', () => {
+    let count = 0
+    for (let i = 0; i < 100; i++) {
+      const community = new Community()
+      const a = new Person(community)
+      const b = new Person()
+      const c = new Person()
+      const d = new Person()
+      const id = community.startPolycule(a, b, c)
+      commitAdultery(c, d, community)
+      const records = community.polycules[id].history.get({ tag: 'adultery' })
+      if (records[0].outcome === 'recovery') count++
+    }
+    expect(count).toBeGreaterThan(50)
   })
 })
